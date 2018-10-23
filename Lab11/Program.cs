c60f851e6310706927651075f34c4dd4357c25f8
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-10-23 14:32:29 -0400

    intial commmit

---
+++ Lab11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+            //store a list of 10 movies and display them by category.
+            //user enters category of Animated, Drama, Horror, SciFi
+            //ask user to continue
+
+            //Movie class should provide two private fields: title and category. 
